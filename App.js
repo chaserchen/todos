f901df828f418fb9ad25abf7acc912a36d14701d
Guo Chen <chenguo@honovation.com>
2016-07-08 17:09:10 +0800

    refactor: code structure

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters } from './actions';
-import AddTodo from './AddTodo';
-import TodoList from './TodoList';
-import Footer from './Footer';
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
